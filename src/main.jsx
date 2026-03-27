7bdadc4dada24aef79652de951531af0eff6fbf2
sandeep140908 <sandeep1212n@gmail.com>
2026-03-27 19:03:52 +0530

    error changed

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 🔥 FIX GITHUB PAGES REDIRECT (PLACE BEFORE RENDER)
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.pathname) {
+  window.history.replaceState(null, null, redirect);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
